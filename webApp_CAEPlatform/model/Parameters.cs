342d24df1e6e4f4edad41553e41b2af38fb38600
Vladislav <chinnov.vlad@gmail.com>
2023-12-27 14:30:26 +0300

    переопределение модели

--- webApp_CAEPlatform/model/Parameters.cs
+++ webApp_CAEPlatform/model/Parameters.cs
@@ -12,4 +12,10 @@ public class Parameters
     public int L2 { get; set; }
     [JsonPropertyName("l3")]
     public int L3 { get; set; }
+
+    public override string? ToString()
+    {
+        var str = $"H1:{H1.ToString()}; H3:{H3.ToString()}; L2:{L2.ToString()}; L3:{L3.ToString()}";
+        return str;
+    }
 }
