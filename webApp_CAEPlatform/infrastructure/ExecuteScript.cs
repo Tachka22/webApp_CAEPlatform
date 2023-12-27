84d1eaffb6a6b367e62f7e2198ba82a7c80dc3b2
Vladislav <chinnov.vlad@gmail.com>
2023-12-27 15:06:33 +0300

    добавил пути на исполнение скриптов

--- webApp_CAEPlatform/infrastructure/ExecuteScript.cs
+++ webApp_CAEPlatform/infrastructure/ExecuteScript.cs
@@ -8,9 +8,33 @@ public class ExecuteScript
     #region Пути к файлам логирования терминала
     private const string pathTo_TerminalLogs = "";
     #endregion
+    #region Пути к исполняемым файлам.
+    /// <summary>
+    /// Скрипт запуска решателя.
+    /// </summary>
+    private const string pathTo_RunSolverScript = "";
+    /// <summary>
+    /// Скрипт очистки прошлых решений.
+    /// </summary>
+    private const string pathTo_ClearLastSolutionScript = "";
+    /// <summary>
+    /// Скрипт создания сетки модели.
+    /// </summary>
+    private const string pathTo_CreateMesh = "";
+    /// <summary>
+    /// Скрипт запуска приложения: "ParaView Desktop"
+    /// </summary>
+    private const string pathTo_OpenParaViewDesktop = "";
+    #endregion
     
     private Process myProcess;
     
+    
+    
+    /// <summary>
+    /// Базовый метод исполнения скриптов.
+    /// </summary>
+    /// <param name="path">скрипт</param>
     private async Task ExecuteScriptBase(string path)
     {
         try
@@ -41,6 +65,11 @@ public class ExecuteScript
             Console.WriteLine(e.Message);
         }
     }
+    /// <summary>
+    /// Запись вывода терминала.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void ProcessOnOutputDataReceived(object sender, DataReceivedEventArgs e)
     {
         using var writer = new StreamWriter(pathTo_TerminalLogs, true);
