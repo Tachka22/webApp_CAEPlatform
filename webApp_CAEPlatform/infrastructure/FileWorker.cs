b7c50bc202183760412fbcaf76a624a4e1483335
Vladislav <chinnov.vlad@gmail.com>
2023-12-27 14:40:40 +0300

    метод изменения координат для построения сетки

--- webApp_CAEPlatform/infrastructure/FileWorker.cs
+++ webApp_CAEPlatform/infrastructure/FileWorker.cs
@@ -21,4 +21,24 @@ public class FileWorker
     private const string file_StreamingBuffer = "";
     #endregion
     
+    /// <summary>
+    /// Изменить значения координат для построения сетки.
+    /// </summary>
+    public async Task ChangeVarInFile(int changeL2,int changeL3,int changeH1,int changeH3)
+    {
+        string tempText;
+        //Чтение подготовленного файла.
+        using (var reader = new StreamReader(file_ChangeBlockMeshString))
+            tempText = await reader.ReadToEndAsync();
+        //Изменение.
+        tempText = tempText.Replace("change_l2", changeL2.ToString())
+            .Replace("change_l3", changeL3.ToString())
+            .Replace("change_h1", changeH1.ToString())
+            .Replace("change_h3", changeH3.ToString());
+        //Запись значений.
+        await using (var writer = new StreamWriter(file_ChangeBlockMeshString, false))
+            await writer.WriteLineAsync(tempText);
+    }
+    
+
 }
