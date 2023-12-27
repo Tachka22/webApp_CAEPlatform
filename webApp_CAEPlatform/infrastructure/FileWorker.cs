2fc8a0df2cb8d6bc8067702444a5f3646f09b844
Vladislav <chinnov.vlad@gmail.com>
2023-12-27 14:36:21 +0300

    пути к файлам и директориям

--- webApp_CAEPlatform/infrastructure/FileWorker.cs
+++ webApp_CAEPlatform/infrastructure/FileWorker.cs
@@ -2,5 +2,23 @@ namespace webApp_CAEPlatform.infrastructure;
 
 public class FileWorker
 {
+    #region Пути к файлам.
+    /// <summary>
+    /// Путь к перезаписываемому файлу.
+    /// </summary>
+    private const string file_ChangeBlockMeshString = "";
+    /// <summary>
+    /// Путь к файлу со стандартными настройками.
+    /// </summary>
+    private const string file_DefaultBlockMeshString = "";
+    /// <summary>
+    /// Путь к файлу записи вывода терминала.
+    /// </summary>
+    private const string file_TerminalLogs = "";
+    /// <summary>
+    /// Стриминговый буфер.
+    /// </summary>
+    private const string file_StreamingBuffer = "";
+    #endregion
     
 }
